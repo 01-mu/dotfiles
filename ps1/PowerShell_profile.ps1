38217823f7e87f0dc1ac06a857212e642644d613
Masato Umeno <masato.umeno.818@gmail.com>
2025-08-09 18:24:52 +0900

    add zoxide

--- ps1/PowerShell_profile.ps1
+++ ps1/PowerShell_profile.ps1
@@ -7,7 +7,14 @@ function lg { Get-ChildItem -Force | Select-String $args }
 function .. { Set-Location .. }
 function ... { Set-Location ..\.. }
 
+Set-Alias j z
 
 if (Test-Path "$PWD\workspace") {
   Set-Location "$PWD\workspace"
 }
+
+# For zoxide v0.8.0+
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell | Out-String)
+})
