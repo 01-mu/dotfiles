80ce259f9a03aa6cbd2b0bdd2b7264ee49e0161b
mu <01-mu@users.noreply.github.com>
2026-01-10 00:09:40 +0900

    chore: update init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,10 @@
--- intentionally empty
+-- Line numbers
+vim.opt.number = true
+
+-- Indentation
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+
+-- Enable mouse
+vim.opt.mouse = "a"
