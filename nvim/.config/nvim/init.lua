e027c109820b126e9a3e05d7fad3211b615c47ca
mu <01-mu@users.noreply.github.com>
2026-01-10 00:18:59 +0900

    feat(nvim): add lazy.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -8,3 +8,30 @@ vim.opt.tabstop = 2
 
 -- Enable mouse
 vim.opt.mouse = "a"
+
+-- lazy.nvim bootstrap -----------------------------------------
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- 安定版を使う
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+-- ------------------------------------------------------------
+
+require("lazy").setup({
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup()
+    end,
+  },
+})
+
