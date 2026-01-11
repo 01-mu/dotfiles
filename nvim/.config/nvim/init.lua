13b72868552021e1b1626820e453e0bc03281100
mu <01-mu@users.noreply.github.com>
2026-01-11 10:09:03 +0900

    feat(nvim): add lazygit plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -68,4 +68,13 @@ require("lazy").setup({
       require("gitsigns").setup({})
     end,
   },
+
+  -- Lazygit
+  {
+    "kdheepak/lazygit.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "Open lazygit" })
+    end,
+  },
 })
