cdc4b8532ad266a87c5db500b93d0a7b2dc1c26b
01-mu <01-mu@users.noreply.github.com>
2026-04-15 09:34:38 +0900

    chore: remove archived configs

--- archive/nvim/.config/nvim/after/ftplugin/markdown.lua
+++
@@ -1,3 +0,0 @@
-if vim.treesitter and vim.treesitter.start then
-  pcall(vim.treesitter.start, 0)
-end
--- archive/nvim/.config/nvim/lua/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
--- jj to escape (insert mode)
-vim.keymap.set("i", "jj", "<Esc>", { noremap = true })
-
-vim.keymap.set("n", "<C-h>", "<cmd>bprev<CR>")
-vim.keymap.set("n", "<C-l>", "<cmd>bnext<CR>")
